e7321f05163cddb81469d999f97457f2349eab67
Doug Todd <doug.todd@hey.com>
2025-06-08 15:03:25 +0100

    feat: Added "logout" functionality

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
-import 'package:at_client_mobile/at_client_mobile.dart';
+import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
+import 'package:atmail/app.dart';
+import 'package:atmail/router/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // * Once the onboarding process is completed you will be taken to this screen
 class HomeScreen extends StatelessWidget {
@@ -15,7 +18,31 @@ class HomeScreen extends StatelessWidget {
     // * From this widget onwards, you are fully onboarded!
     // * You can use the AtClient instance to perform operations as the onboared atSign
     return Scaffold(
-      appBar: AppBar(title: const Text('What\'s my current @sign?')),
+      appBar: AppBar(
+        title: const Text('What\'s my current @sign?'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              final resetResult = await AtOnboarding.reset(
+                context: context,
+                config: AtOnboardingConfig(
+                  atClientPreference: context.read<AppDependencies>().atClientPreferences,
+                  rootEnvironment: RootEnvironment.Production,
+                ),
+              );
+              switch (resetResult) {
+                case AtOnboardingResetResult.cancelled:
+                // Do nothing
+                case AtOnboardingResetResult.success:
+                  if (context.mounted) {
+                    OnboardingRoute().go(context);
+                  }
+              }
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           children: [
